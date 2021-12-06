c2a75e6a1e5fb0ba409cfe396f0b5e9b68d3096d
arHSM <hanseungmin.ar@gmail.com>
2021-12-06 16:37:26 +0000

    feat: NamedDictMeta class
    `NamedDictMeta` add makes life even easier with stuff such as no more anonymous keys and musch more...

--- README.md
+++ README.md
@@ -23,17 +23,60 @@ Well its as simple as this -
 >>> # initialise it just like you would do with `dict()`
 >>> data = Data(value_a=1, value_b="hi!")
 >>> print(data)
-{"value_a": 1, "value_b": "hi!"}
+Data {"value_a": 1, "value_b": "hi!"}
 >>> print(data.value_a)
 1
 >>> print(data.value_b)
 "hi!"
 ```
 
-> NOTE: These type hints currently mean nothing!\
-> defining certain keys doesn't mean that you cannot have "anonymous" keys.
-> I do have plans on releasing an extention that provides some utilities for
-> `NamedDict`s but not now.
+Points to note:
+
+1. Attributes that conflict with the methods in `dict` object such as `items`, `keys`, `values`, etc... cannot be accessed by `instance.items` instead you need to do `instance["items"]`.
+
+2. You need to add `__slots__ = ()` to prevent the creation of `__dict__` (Which you don't need and want, *trust me*).
+
+> **NOTE**: These type hints mean nothing!
+
+## Extending `NamedDict`s
+
+With the new `NamedDictMeta` you can extend your NamedDicts.
+
+```pycon
+>>> form nameddict import NamedDict, NamedDictMeta
+>>>
+>>>
+>>> class Data(NamedDict, metaclass=NamedDictMeta):
+...     val_1: str
+...     val_2 = 5
+...     val_3: bool = False
+...
+>>> data = Data()
+>>> data
+Data {"val_1": None, "val_3": False}
+>>> data.val_2 = 5
+# Simplified error
+AttributeError: Data has no attribute val_2.
+>>> data.val_3
+True
+>>> data.val_2
+# Simplified error
+KeyError: 'val_2'
+```
+
+Points to note:
+
+1. Type hints still don't mean anything, you can always pass in a different type of object.
+
+2. Attribute without an annotation will be ignored if there are attributes with annotations present (look into the implementation for better understanding).
+
+3. Default values is `None` unless explicitly set.
+
+4. You do not need to add the `__slots__` line, because the metaclass handles that.
+
+Why use `(NamedDict, metaclass=NamedDictMeta)`, instead of handling it in the meta class?
+
+- Long story short, it's mainly for type-checkers to stop being annoying.
 
 ## How to install?
 
--- nameddict.py
+++ nameddict.py
@@ -1,6 +1,14 @@
-__all__ = ("NamedDict",)
+__all__ = ("NamedDict", "NamedDictMeta")
 
-from typing import Any
+from typing import (
+    Annotated,
+    Any,
+    Dict,
+    NamedTuple,
+    Optional,
+    Union,
+    get_origin
+)
 
 
 class NamedDict(dict):
@@ -33,12 +41,65 @@ class NamedDict(dict):
     "hi!"
     ```
     """
-    __slots__ = ()
+    __slots__ = {}
+    __mapping__: Dict[str, Any]
+
+    def __init__(self, **kwargs):
+        try:
+            for name, default in self.__mapping__.items():
+                value = kwargs.get(name, default)
+                self.__setattr__(name, value)
+        except AttributeError:
+            super().__init__(**kwargs)
 
     def __getattribute__(self, __name: str) -> Any:
         try:
             return super().__getattribute__(__name)
         except AttributeError:
-            return super().__getitem__(__name)
+            try:
+                if __name in self.__mapping__:
+                    return super().__getitem__(__name)
+                else:
+                    raise AttributeError(
+                        f"{self.__class__.__name__} has no attribute {__name}."
+                    )
+            except AttributeError:
+                return super().__getitem__(__name)
 
-    __setattr__ = dict.__setitem__
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        try:
+            if __name in self.__mapping__:
+                return super().__setitem__(__name, __value)
+            else:
+                raise AttributeError(
+                    f"{self.__class__.__name__} has no attribute {__name}."
+                )
+        except AttributeError:
+            return super().__setitem__(__name, __value)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__} {super().__repr__()}"
+
+
+class NamedDictMeta(type):
+    
+    def __new__(cls, name: str, bases: tuple, namespace: dict) -> NamedDict:
+
+        namespace["__slots__"] = set()
+        namespace["__mapping__"] = {}
+
+        try:
+            temp_ns = namespace["__annotations__"].copy()
+        except KeyError:
+            return type.__new__(cls, name, bases, namespace)
+
+        for attribute in temp_ns:
+            default = None
+            if attribute in namespace:
+                default = namespace.pop(attribute)
+            
+            namespace["__mapping__"][attribute] = default
+
+        nd_dict = type.__new__(NamedDictMeta, name, bases, namespace)
+
+        return nd_dict
